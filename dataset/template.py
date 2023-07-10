30fe9071d49ecf0150b88dcf417c549e7dd397c0
wphu <wphu@aibee.com>
2023-07-10 11:47:28 +0800

    增加regioncontext模板；增加对in/out door_id找不到对应door的处理(配置和GT事件中的id没对齐导致)

--- dataset/template.py
+++ dataset/template.py
@@ -40,20 +40,22 @@ class StoreInoutTemplate(Template):
             template = f.read()
 
         # Door caption
-        door_in = self.area_descriptor.door_region[str(self.event['in_door_id'])]
-        door_out = self.area_descriptor.door_region[str(self.event['out_door_id'])]
-        in_door_name = "Door" + door_in.id
-        out_door_name = "Door" + door_out.id
+        if str(self.event['in_door_id']) not in self.area_descriptor.door_region and str(self.event['out_door_id']) not in self.area_descriptor.door_region:
+           return ""
+        door_in = self.area_descriptor.door_region[str(self.event['in_door_id'])] if str(self.event['in_door_id']) in  self.area_descriptor.door_region else None
+        door_out = self.area_descriptor.door_region[str(self.event['out_door_id'])] if str(self.event['out_door_id']) in self.area_descriptor.door_region else None
+        in_door_name = "Door" + door_in.id if door_in else "Door"
+        out_door_name = "Door" + door_out.id if door_out else "Door"
 
         # 如果self.ts不为None, 则根据ts来判断是进店还是出店
         inout = "经过"
         door = in_door_name
         if self.ts:
             # ts 在开始时间附近(前后10s)则标记inout为'进入'，结束时间附近标记为'走出'
-            if abs(self.ts - self.event['start_time']) < STORE_INOUT_NEAR_TIME * 2:
+            if door_in and abs(self.ts - self.event['start_time']) < STORE_INOUT_NEAR_TIME * 2:
                 inout = "进入"
                 door = in_door_name
-            elif abs(self.ts - self.event['end_time']) < STORE_INOUT_NEAR_TIME * 2:
+            elif door_out and abs(self.ts - self.event['end_time']) < STORE_INOUT_NEAR_TIME * 2:
                 inout = "走出"
                 door = out_door_name
         
@@ -240,3 +242,38 @@ class BboxEmbeddingTemplate(Template):
         return template
     
 
+class RegionContextTemplate(Template):
+
+    def __init__(self, template_file, event, area_descriptor=None):
+        super().__init__(template_file, event, area_descriptor)
+
+    @wrapper_str
+    def __str__(self):
+        with open(self.template_file, 'r') as f:
+            template = f.read()
+        
+        region = self.event['region']
+
+        if self.event["event_type"] == "REGION_INOUT" and \
+            self.event["region_type"] == "CAR":  # CAR_INOUT
+            region_name = "Car"
+        elif self.event["event_type"] == "REGION_VISIT":
+            region_name = "Car"
+        elif self.event['event_type'] == "STORE_INOUT":
+            region_name = "Door"
+        elif self.event["event_type"] == "REGION_INOUT" and \
+            self.event["region_type"] == "INTERNAL_REGION":
+            region_name = "InternalRegion"
+        else:
+            region_name = "Region"
+
+        coords_round = []
+        for coord in region.coords:
+            coords_round.append([round(coord[0], 2), round(coord[1], 2)])
+        template = template.format(
+            region_name,
+            region.id, 
+            coords_round
+        )
+        
+        return template
